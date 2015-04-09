067a220cc76f698c02fbce671851f155bffc84c7
Kyle Weisel <kyleweisel@gmail.com>
2015-04-09 10:08:20 -0400

    Implemented generateRules() method

--- WordMorph.py
+++ WordMorph.py
@@ -49,8 +49,25 @@ def generateRules(state):
 
     rules = []
 
+    for i in range(0, len(state.dictionary)):
 
+        if numMatchedChars(state.currentState, state.dictionary[i]) == 4:
 
+            # Find the changed character
+            stateAsList = list(state.currentState)
+            matchAsList = list(state.dictionary[i])
+
+            changeIndex = -1
+            changeChar = ""
+
+            for j in range(0, len(stateAsList)):
+                if stateAsList[j] == matchAsList[j]:
+                    changeIndex = j
+                    changeChar = matchAsList[j]
+
+            rules.append(Rule(changeIndex, changeChar))
+
+    return rules
 
 
 def describeState(state):
@@ -59,6 +76,19 @@ def describeState(state):
 
 def describeRule(rule):
     print "The rule will change the character at index " + str(rule.index) + " to " + str(rule.char)
+
+
+def numMatchedChars(a, b):
+    if len(a) == len(b):
+        aList = list(a)
+        bList = list(b)
+        numMatches = 0
+        for i in range(0, len(aList)):
+            if aList[i] == bList[i]:
+                numMatches += 1
+        return numMatches
+    else:
+        return -1
 
 
 def main():
